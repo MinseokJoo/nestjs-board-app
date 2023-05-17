eb499ef9b79b5fb844d950e18c2f5201baee9fa9
Joominseok <jumseok12@gmail.com>
2023-05-17 20:48:51 +0900

    로컬 board 작성 코드 추가

---
+++ src/boards/boards.model.ts
@@ -0,0 +1,18 @@
+// 모델을 정의하기 위해서는 Class를 이용하거나 Interface를 이용하면 된다.
+// interface => 변수의 타입만을 체크합니다.
+// classes => 변수의 타입도 체크하고 인스턴스 또한 생성할 수 있습니다.
+// board의 구조만 정의하기 위해 interface를 사용할게용...
+// board status : 공개 게시물인지, 비공개 게시물인지의 상태 이외에는 나오면 안되기 때문에
+// 두가지의 상태만 나올 수 있게 하기 위해서 enumeration을 이용하겠습니다.
+
+export interface Board {
+  id: string;
+  title: string;
+  description: string;
+  status: BoardStatus;
+}
+
+export enum BoardStatus {
+  PUBLIC = 'PUBLIC',
+  PRIVATE = 'PRIVATE',
+}
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
-  private boards = [];
+  private boards: Board[] = [];
 
-  getAllBoards() {
+  /*타입을 정의해주면 좋은 이유?
+    타입을 정의해주는 것은 선택사항입니다.
+    하지만 이렇게 타입을 정의해주므로서 원하는 타입과 다른 코드를 사용할 시 에러가 발생합니다.
+    그리고 코드를 읽는 입장에서 더 코드를 쉽게 이해하며 읽을 수 있습니다. (가독성 readable)*/
+
+  getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  /*게시물 ID는 어떻게 처리해야할까?
+  ID는 모든 게시물에 유니크 해야합니다.
+  그래서 만약 데이터베이스에 데이터를 넣어줄 때는 데이터베이스가 알아서 유니크한 값을 줍니다.
+  하지만 현재는 데이터베이스를 사용하지 않기 때문에 임의로 유니크한 값을 줘야 합니다.
+  이때 여러 방법을 쓸 수 있지만 uuid 모듈을 이용해서 유니크한 값을 주겠습니다.*/
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+
+    return board;
   }
 }
